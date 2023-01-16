55c16fdc4a566d1d1b66225649e10a49ba153444
Velnbur <kirillbaybula@gmail.com>
2023-01-16 11:00:28 +0200

    Add some docs with examples to metamask lib

--- libs/metamask/src/lib.rs
+++ libs/metamask/src/lib.rs
@@ -3,6 +3,7 @@ pub mod transaction;
 
 use ethereum::EthereumProvider;
 use js_sys::Array;
+pub use transaction::Transaction;
 use wasm_bindgen::JsValue;
 use web_sys::Window;
 
@@ -26,6 +27,21 @@ impl Metamask {
         Ok(Metamask { eth })
     }
 
+    /// Retuns addresses of accessible accounts for currently connected metamask.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use wasm_bindgen::UnwrapThrowExt;
+    ///
+    /// let window = web_sys::window().expect_throw("window is undefined");
+    /// let metamask =
+    ///     Metamask::from_window(&window).expect_throw("failed to create metamask client");
+    /// let accounts = metamask
+    ///     .accounts()
+    ///     .await
+    ///     .expect_throw("failed to get accounts");
+    /// ```
     pub async fn accounts(&self) -> Result<Vec<String>, Error> {
         const ETH_REQUEST_ACCOUNTS: &str = "eth_requestAccounts";
 
@@ -59,5 +75,42 @@ impl Metamask {
             js_sys::Reflect::get(&window, &ETHEREUM_PROVIDER.into()).unwrap_or(JsValue::UNDEFINED);
 
         !eth.is_undefined()
+    }
+
+    /// Send a transaction request to metamask, and return a hash of the transaction.
+    ///
+    /// ```rust
+    /// use wasm_bindgen::UnwrapThrowExt;
+    ///
+    /// let window = web_sys::window().expect_throw("window is undefined");
+    /// let metamask = Metamask::from_window(&window)
+    ///                         .expect_throw("failed to create metamask client");
+    ///
+    /// let tx = Transaction {
+    ///     to: "0xA1fFe6f9C12Fc0d42740A7a6d1538daA005EfBC8".to_owned(),
+    ///     from: "0xA1fFe6f9C12Fc0d42740A7a6d1538daA005EfBC8".to_owned(),
+    ///     data: Vec::default(),
+    ///     chain_id: 5,
+    /// };
+    ///
+    /// let tx_hash = metamask.send_transaction(&tx)
+    ///                       .await
+    ///                       .expect_throw("failed to send tx");
+    /// ```
+    pub async fn send_transaction(&self, tx: &Transaction) -> Result<String, Error> {
+        const ETH_SEND_TX: &str = "eth_sendTransaction";
+
+        let ctx = JsValue::default();
+
+        let tx_hash = self
+            .eth
+            .request(
+                &ctx,
+                ETH_SEND_TX.to_owned(),
+                vec![serde_wasm_bindgen::to_value(&tx).unwrap()],
+            )
+            .await?;
+
+        Ok(tx_hash.as_string().unwrap_or_default())
     }
 }
